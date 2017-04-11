44dadff425f21af3b16e6c4460b7c6a693a3b8dc
Sabremoscumplir <roman.sugo@gmail.com>
2017-04-11 17:29:17 -0300

    Update draw.js

--- draw.js
+++ draw.js
@@ -33,7 +33,7 @@ var svg = d3.select("body").append("svg")
 
 svg.call(tip);
 
-d3.csv("https://github.com/Sabremoscumplir/365despues/blob/master/aster_data.csv", function(error, data) {
+d3.csv("http://github.com/Sabremoscumplir/365despues/blob/master/aster_data.csv", function(error, data) {
 
   data.forEach(function(d) {
     d.id     =  d.id;
